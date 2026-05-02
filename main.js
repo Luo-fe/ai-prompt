7cedbe5bdca87874c242e7e679760e709700839c
Luo-fe <2564118019@qq.com>
2026-05-02 18:27:42 +0800

    perf: 内存优化 - 翻译字典常量化、缓存上限、resize防抖、通知限制、sandbox启用

--- main.js
+++ main.js
@@ -16,7 +16,9 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      webSecurity: true
+      webSecurity: true,
+      sandbox: true,
+      backgroundThrottling: true
     }
   });
 
